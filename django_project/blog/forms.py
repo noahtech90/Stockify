2b0b2df43aadb4a6f66088402b4c3fc374996c97
Noah <noahhodge2@gmail.com>
2020-09-21 19:44:17 -0700

    Adjusted Searching Form Display on small screens

--- django_project/blog/forms.py
+++ django_project/blog/forms.py
@@ -3,6 +3,7 @@ from .models import Security
 
 class TickerForm(forms.ModelForm):
     ticker = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Enter "AAPL" and search'}))
+    ticker.widget.attrs.update(size='36')
     class Meta:
         model = Security
         fields = ['ticker']
