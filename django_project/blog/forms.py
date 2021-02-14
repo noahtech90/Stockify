98ad0fe57ef5ab165fb2518f9c2b3ce2489bfeff
Noah <noahhodge2@gmail.com>
2021-02-13 19:27:55 -0800

    form features

--- django_project/blog/forms.py
+++ django_project/blog/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from .models import Security
 
 class TickerForm(forms.ModelForm):
-    ticker = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Enter "AAPL" and search'}))
-    #ticker.widget.attrs.update(size='50%')
+    ticker = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Enter "BA" and search', 'class': 'ticker-field'}))
     class Meta:
         model = Security
         fields = ['ticker']
